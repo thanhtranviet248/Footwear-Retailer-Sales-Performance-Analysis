dfacded225476c20042d0f8d0ea93af1c77eee81
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-23 23:41:42 +0200

    Update setup_calendar_table.sql

--- setup_calendar_table.sql
+++ setup_calendar_table.sql
@@ -4,12 +4,12 @@ CREATE TABLE calendar
     year        	VARCHAR(4) 	NOT NULL, -- 2020
     month       	VARCHAR(2) 	NOT NULL, -- 1, 2, 3,..., 12
     week        	VARCHAR(2) 	NOT NULL, -- 1, 2, 3,..., 52
-	lunar_ny		VARCHAR(3)	NOT NULL, -- Yes/No
-    season      	VARCHAR(10), -- 2018T11-02
-	month_3char 	VARCHAR(3), -- 001,..., 012
-	week_2char		VARCHAR(2), -- 01,...,52
-	pk_date 		VARCHAR(9), -- primary key as date (202000101)
-	PRIMARY KEY (pk_date)
+    lunar_ny    	VARCHAR(3)	NOT NULL, -- Yes/No
+    season      	VARCHAR(10), 		  -- 2018T11-02
+    month_3char 	VARCHAR(3), 		  -- 001,..., 012
+    week_2char		VARCHAR(2), 		  -- 01,...,52
+    pk_date 		VARCHAR(9), 		  -- primary key as date (202000101)
+    PRIMARY KEY (pk_date)
 );
 
 -- Create the trigger function to populate month_3char
