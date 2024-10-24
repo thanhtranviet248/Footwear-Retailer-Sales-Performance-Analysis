41239d8cbb2e9a3d2b3ee92c22fa1c1a58095c7c
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-24 23:45:15 +0200

    Update setup_product_table.sql

--- setup_product_table.sql
+++ setup_product_table.sql
@@ -1,25 +1,47 @@
 -- Create product table
 CREATE TABLE product
 (
-	sku 			VARCHAR(14)	NOT NULL,
-	stylecolor 		VARCHAR(12) 	NOT NULL,
-	style 			VARCHAR(9) 	NOT NULL,
-	color 			VARCHAR(3),
-	colorgroup 		TEXT,
-	retailprice 		NUMERIC 	NOT NULL,
-	pricegroup 		TEXT,
-	brand 			TEXT 		NOT NULL,
-	gender 			VARCHAR(3) 	NOT NULL,
-	productgroup 		VARCHAR(3) 	NOT NULL,
-	detailproductgroup 	VARCHAR(6) 	NOT NULL,
-	size 			VARCHAR(2),
-	sizerange 		VARCHAR(5),
-	agegroup 		TEXT,
-	lifestyle 		TEXT,
-	launchseason 		VARCHAR(4),
+	sku VARCHAR(14)	NOT NULL,
+	stylecolor VARCHAR(12) NOT NULL,
+	style VARCHAR(9) NOT NULL,
+	color VARCHAR(3),
+	colorgroup TEXT,
+	retailprice NUMERIC NOT NULL,
+	pricegroup TEXT,
+	brand TEXT NOT NULL,
+	gender VARCHAR(3) NOT NULL,
+	productgroup VARCHAR(3) NOT NULL,
+	detailproductgroup VARCHAR(6) NOT NULL,
+	size VARCHAR(2),
+	sizerange VARCHAR(5),
+	agegroup TEXT,
+	lifestyle TEXT,
+	launchseason VARCHAR(4),
 	PRIMARY KEY (sku)
 )
 
+-- Create trigger function to clean product table
+CREATE OR REPLACE FUNCTION clean_product() RETURNS TRIGGER AS $$
+BEGIN
+	-- clean colorgroup
+	IF NEW.colorgroup = 'T?I' THEN NEW.colorgroup := 'Dark';
+	ELSIF NEW.colorgroup = 'SÁNG' THEN NEW.colorgroup := 'Bright';
+	ELSIF NEW.colorgroup = '?EN' THEN NEW.colorgroup := 'Black';
+	ELSIF NEW.colorgroup = 'TR?NG' THEN NEW.colorgroup := 'White';
+	ELSIF NEW.colorgroup = 'NHI?U MÀU' THEN NEW.colorgroup := 'Multicolor';
+	ELSE NEW.colorgroup := 'Others';
+	END IF;
+	-- clean agegroup
+	NEW.agegroup = REPLACE(REPLACE(NEW.agegroup, '??n', 'to'), ' tu?i', '');
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger to clean product table
+CREATE TRIGGER clean_product
+BEFORE INSERT OR UPDATE ON product
+FOR EACH ROW
+EXECUTE FUNCTION clean_product();
 
 -- Copy data to product table
 COPY product
@@ -27,18 +49,5 @@ FROM 'D:\personal_project\retail_dashboard\product.csv'
 DELIMITER ','
 CSV HEADER;
 
--- Clean product table
-UPDATE product
-	SET colorgroup =
-		CASE
-			WHEN colorgroup = 'T?I' THEN 'Dark'
-			WHEN colorgroup = 'SÁNG' THEN 'Bright'
-			WHEN colorgroup = '?EN' THEN 'Black'
-			WHEN colorgroup = 'TR?NG' THEN 'White'
-			WHEN colorgroup = 'NHI?U MÀU' THEN 'Multicolor' 
-			ELSE 'Others'
-		END,
-		agegroup = REPLACE(REPLACE(agegroup, '??n', 'to'), ' tu?i', '');
-
-
-
+-- Recheck product table data
+SELECT * FROM product LIMIT 10;
