333f0de9f3e341489dff223eb61c542d64694ad7
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-24 14:43:40 +0200

    Update setup_calendar_table.sql

--- setup_calendar_table.sql
+++ setup_calendar_table.sql
@@ -2,57 +2,50 @@
 CREATE TABLE calendar
 (
     year        	VARCHAR(4) 	NOT NULL, -- 2020
-    month       	VARCHAR(2) 	NOT NULL, -- 1, 2, 3,..., 12
+    month       	VARCHAR(3) 	NOT NULL, -- 1, 2, 3,..., 12
     week        	VARCHAR(2) 	NOT NULL, -- 1, 2, 3,..., 52
-    lunar_ny    	VARCHAR(3)	NOT NULL, -- Yes/No
-    season      	VARCHAR(10), 		  -- 2018T11-02
-    month_3char 	VARCHAR(3), 		  -- 001,..., 012
-    week_2char		VARCHAR(2), 		  -- 01,...,52
-    pk_date 		VARCHAR(9), 		  -- primary key as date (202000101)
-    PRIMARY KEY (pk_date)
+    lunarnewyear	VARCHAR(3)	NOT NULL, -- Yes/No
+    season      	VARCHAR(10),          -- 2018T11-02
+    pk_yearweek		VARCHAR(6),           -- 202001
+    PRIMARY KEY (pk_yearweek)
 );
 
--- Create the trigger function to populate month_3char, week_2char, pk_date
-CREATE OR REPLACE FUNCTION update_calendar() RETURNS TRIGGER AS $$
+-- Create the trigger function to populate month_3char
+CREATE OR REPLACE FUNCTION clean_calendar() RETURNS TRIGGER AS $$
 BEGIN
-	-- update month_3char
+	-- update month
     IF LENGTH(NEW.month) = 1 THEN
-        NEW.month_3char := CONCAT('00', NEW.month);
+        NEW.month := CONCAT('00', NEW.month);
     ELSE
-        NEW.month_3char := CONCAT('0', NEW.month);
+        NEW.month := CONCAT('0', NEW.month);
     END IF;
 	-- update week_2char
 	IF LENGTH(NEW.week) = 1 THEN
-        NEW.week_2char := CONCAT('0', NEW.week);
+        NEW.week := CONCAT('0', NEW.week);
     ELSE
-        NEW.week_2char := NEW.week;
+        NEW.week := NEW.week;
     END IF;
 	-- update pk_date
-	NEW.pk_date := CONCAT(NEW.year, NEW.month_3char, NEW.week_2char);
+	NEW.pk_yearweek := CONCAT(NEW.year, NEW.week);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Create the trigger
-CREATE TRIGGER update_calendar
+CREATE TRIGGER clean_calendar
 BEFORE INSERT OR UPDATE ON calendar
 FOR EACH ROW
-EXECUTE FUNCTION update_calendar();
+EXECUTE FUNCTION clean_calendar();
 
 -- Update data into calendar
-COPY calendar (year, month, week, lunar_ny, season)
+COPY calendar (year, month, week, lunarnewyear, season)
 FROM 'D:\personal_project\retail_dashboard\calendar.csv'
 DELIMITER ','
 CSV HEADER;
 
--- Clear null value
+-- Clean null value in season
 DELETE FROM calendar
 WHERE season IS NULL;
 
--- Update lunar_ny to binary
-UPDATE calendar
-SET lunar_ny =
-	CASE
-		WHEN lunar_ny = 'No' THEN 0
-		ELSE 1
-	END;
+-- Recheck calendar table
+select * from calendar;
