f20cd034c4997f5040bdb3305b276cf17663661a
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-24 23:50:39 +0200

    Update setup_distributionchannel_and_retailstore_table.sql

--- setup_distributionchannel_and_retailstore_table.sql
+++ setup_distributionchannel_and_retailstore_table.sql
@@ -1,54 +1,54 @@
 -- Create distribution channel table
 CREATE TABLE distributionchannel
 (
-	customercode	TEXT		NOT NULL,
+	customercode		TEXT		NOT NULL,
 	storecode		TEXT		NOT NULL,
 	b2bb2c			TEXT		NOT NULL,
 	channel			TEXT		NOT NULL,
 	citylevel		TEXT		NOT NULL,
-	storelevel		TEXT				,
-	areagroup		TEXT				,
-	urbanization	TEXT		NOT NULL,
+	storelevel		TEXT			,
+	areagroup		TEXT			,
+	urbanization		TEXT		NOT NULL,
 	branch			TEXT		NOT NULL,
-	showroomarea	DECIMAL				,
-	warehousearea	DECIMAL				,
-	openmonth		VARCHAR(2)			,
-	openyear		VARCHAR(4)			,
-	Active			INT			NOT NULL,
+	showroomarea		DECIMAL			,
+	warehousearea		DECIMAL			,
+	openmonth		VARCHAR(2)		,
+	openyear		VARCHAR(4)		,
+	Active			INT		NOT NULL,
 	PRIMARY KEY (customercode)
 );
 
 -- Create trigger function to clean distribution channel table
 CREATE OR REPLACE FUNCTION clean_distributionchannel() RETURNS TRIGGER AS $$
 BEGIN
-	-- clean channel
-	IF 		NEW.channel = 'CHTT' 	THEN NEW.channel := 'Retail Store';
-	ELSIF	NEW.channel = 'ONLINE' 	THEN NEW.channel := 'Online';
+	-- clean channel column
+	IF 	NEW.channel = 'CHTT' 		THEN NEW.channel := 'Retail Store';
+	ELSIF	NEW.channel = 'ONLINE' 		THEN NEW.channel := 'Online';
 	ELSIF	NEW.channel = 'ST' 		THEN NEW.channel := 'Supermarket';
 	ELSE	NEW.channel := 'Wholesales';
 	END IF;
-	-- clean citylevel
-	IF 		NEW.citylevel = 'C?p 1' 	THEN NEW.citylevel := 'Level 1';
+	-- clean citylevel column
+	IF 	NEW.citylevel = 'C?p 1' 	THEN NEW.citylevel := 'Level 1';
 	ELSIF	NEW.citylevel = 'C?p 2' 	THEN NEW.citylevel := 'Level 2';
 	ELSIF	NEW.citylevel = 'C?p TW' 	THEN NEW.citylevel := 'Central Level';
 	ELSE	NEW.citylevel := 'Others';
 	END IF;
-	-- clean storelevel
-	IF 		NEW.storelevel = 'TIÊU BI?U' THEN NEW.storelevel := 'Key store';
+	-- clean storelevel column
+	IF 	NEW.storelevel = 'TIÊU BI?U' 	THEN NEW.storelevel := 'Key store';
 	ELSE	NEW.storelevel := NEW.storelevel;
 	END IF;
-	-- clean urbanization
-	IF 		NEW.urbanization = 'N?i thành'				THEN NEW.urbanization := 'Urban';
-	ELSIF	NEW.urbanization = 'Nông thôn' 			THEN NEW.urbanization := 'Rural';
-	ELSIF	NEW.urbanization = 'TT hành chính kinh t?'	THEN NEW.urbanization := 'Key Location';
+	-- clean urbanization column
+	IF 	NEW.urbanization = 'N?i thành'	THEN NEW.urbanization := 'Urban';
+	ELSIF	NEW.urbanization = 'Nông thôn' 	THEN NEW.urbanization := 'Rural';
+	ELSIF	NEW.urbanization = 'TT hành chính kinh t?' THEN NEW.urbanization := 'Key Location';
 	ELSE	NEW.urbanization := 'Others';
 	END IF;
-	-- clean branch
-	IF 		NEW.branch = 'CNMN'			THEN NEW.branch := 'South Area';
+	-- clean branch column
+	IF 	NEW.branch = 'CNMN'		THEN NEW.branch := 'South Area';
 	ELSIF	NEW.branch = 'CNMB' 		THEN NEW.branch := 'North Area';
-	ELSIF	NEW.branch = 'CNMT'			THEN NEW.branch := 'West Area';
+	ELSIF	NEW.branch = 'CNMT'		THEN NEW.branch := 'West Area';
 	ELSIF	NEW.branch = 'CNTR-TN'		THEN NEW.branch := 'Central-Highland Area';
-	ELSIF	NEW.branch = 'CNCA'			THEN NEW.branch := 'Cambodia';
+	ELSIF	NEW.branch = 'CNCA'		THEN NEW.branch := 'Cambodia';
 	ELSE	NEW.branch := 'Others';
 	END IF;
 	RETURN NEW;
