5b5ca520b4567c700d0bbb159b1093acbd4ebdcc
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-24 12:48:18 +0200

    Update setup_product_table.sql

--- setup_product_table.sql
+++ setup_product_table.sql
@@ -1,22 +1,22 @@
 -- Create product table
 CREATE TABLE product
 (
-	sku VARCHAR(14)	NOT NULL,
-	stylecolor VARCHAR(12) NOT NULL,
-	style VARCHAR(9) NOT NULL,
-	color VARCHAR(3),
-	colorgroup TEXT,
-	retailprice NUMERIC NOT NULL,
-	pricegroup TEXT,
-	brand TEXT NOT NULL,
-	gender VARCHAR(3) NOT NULL,
-	productgroup VARCHAR(3) NOT NULL,
-	detailproductgroup VARCHAR(6) NOT NULL,
-	size VARCHAR(2),
-	sizerange VARCHAR(5),
-	agegroup TEXT,
-	lifestyle TEXT,
-	launchseason VARCHAR(4),
+	sku 			VARCHAR(14)	NOT NULL,
+	stylecolor 		VARCHAR(12) 	NOT NULL,
+	style 			VARCHAR(9) 	NOT NULL,
+	color 			VARCHAR(3),
+	colorgroup 		TEXT,
+	retailprice 		NUMERIC 	NOT NULL,
+	pricegroup 		TEXT,
+	brand 			TEXT 		NOT NULL,
+	gender 			VARCHAR(3) 	NOT NULL,
+	productgroup 		VARCHAR(3) 	NOT NULL,
+	detailproductgroup 	VARCHAR(6) 	NOT NULL,
+	size 			VARCHAR(2),
+	sizerange 		VARCHAR(5),
+	agegroup 		TEXT,
+	lifestyle 		TEXT,
+	launchseason 		VARCHAR(4),
 	PRIMARY KEY (sku)
 )
 
