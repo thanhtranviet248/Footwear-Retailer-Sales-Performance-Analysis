054180ba1763d715e830284d89bf705047ec872e
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-24 23:49:05 +0200

    Update setup_calendar_table.sql

--- setup_calendar_table.sql
+++ setup_calendar_table.sql
@@ -4,29 +4,29 @@ CREATE TABLE calendar
     year        	VARCHAR(4) 	NOT NULL, -- 2020
     month       	VARCHAR(3) 	NOT NULL, -- 1, 2, 3,..., 12
     week        	VARCHAR(2) 	NOT NULL, -- 1, 2, 3,..., 52
-	lunarnewyear	VARCHAR(3)	NOT NULL, -- Yes/No
-    season      	VARCHAR(10),          -- 2018T11-02
-	pk_yearweek		VARCHAR(6),           -- 202001
-	PRIMARY KEY (pk_yearweek)
+    lunarnewyear	VARCHAR(3)	NOT NULL, -- Yes/No
+    season      	VARCHAR(10),              -- 2018T11-02
+    yearweek		VARCHAR(6),               -- 202001
+    PRIMARY KEY (yearweek)
 );
 
 -- Create trigger function to clean calendar table
 CREATE OR REPLACE FUNCTION clean_calendar() RETURNS TRIGGER AS $$
 BEGIN
-	-- clean month
+	-- clean month column
     IF LENGTH(NEW.month) = 1 THEN
         NEW.month := CONCAT('00', NEW.month);
     ELSE
         NEW.month := CONCAT('0', NEW.month);
     END IF;
-	-- clean week_2char
+	-- clean week column
 	IF LENGTH(NEW.week) = 1 THEN
         NEW.week := CONCAT('0', NEW.week);
     ELSE
         NEW.week := NEW.week;
     END IF;
-	-- clean pk_date
-	NEW.pk_yearweek := CONCAT(NEW.year, NEW.week);
+	-- clean yearweek column
+	NEW.yearweek := CONCAT(NEW.year, NEW.week);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
