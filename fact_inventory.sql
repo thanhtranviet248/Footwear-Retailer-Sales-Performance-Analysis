e385616804f16df77b2441bfa19442649662bd8e
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-29 19:46:52 +0100

    Update fact_inventory.sql

--- fact_inventory.sql
+++ fact_inventory.sql
@@ -2,13 +2,13 @@
 CREATE TABLE inventory
 (
 	storecode		VARCHAR(4)	NOT NULL,
-	sku				VARCHAR(14)	NOT NULL,
+	sku			VARCHAR(14)	NOT NULL,
 	year			TEXT		NOT NULL,
 	month			TEXT		NOT NULL,
 	sloc			VARCHAR(4)	NOT NULL,
-	inventoryqty	NUMERIC				,
-	inventoryvalue	NUMERIC				,
-	FOREIGN KEY	(sku)	REFERENCES	product(sku)
+	inventoryqty		NUMERIC			,
+	inventoryvalue		NUMERIC			,
+	FOREIGN KEY (sku) REFERENCES product(sku)
 );
 
 -- Copy data to inventory table
