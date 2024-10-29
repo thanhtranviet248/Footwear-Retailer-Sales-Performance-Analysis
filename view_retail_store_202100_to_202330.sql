0441ff3b781fdce5c519a9c9e68a930f1c5d7ce8
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-29 22:22:53 +0100

    Add files via upload

---
+++ view_retail_store_202100_to_202330.sql
@@ -0,0 +1,15 @@
+-- Create view of sales data and dimensions for retail store channel and time from week 202100 to 202330
+CREATE VIEW retail_202100_202330 AS
+SELECT	a.qty, a.cogsusd, a.revenueusd, 	
+		b.*,
+		c.storecode, c.channel, c.citylevel, c.areagroup, c.urbanization, c.branch, c.active,
+		d.yearweek, d.month, d.year, d.season, d.lunarnewyear
+FROM sales a
+INNER JOIN product b
+ON a.sku = b.sku
+INNER JOIN distributionchannel c
+ON a.customercode = c.customercode
+INNER JOIN calendar d
+ON a.yearweek = d.yearweek
+WHERE c.channel = 'Retail Store' AND CAST(d.yearweek AS NUMERIC) BETWEEN 202100 AND 202330
+ORDER BY d.yearweek ASC;
