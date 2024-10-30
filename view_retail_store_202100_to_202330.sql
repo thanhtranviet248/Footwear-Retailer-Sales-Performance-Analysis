28b4c287dc9f633364836fc63fd8deb6ee8f3195
Thanh Tran Viet <164310620+thanhtranviet248@users.noreply.github.com>
2024-10-30 11:11:30 +0100

    Update view_retail_store_202100_to_202330.sql

--- view_retail_store_202100_to_202330.sql
+++ view_retail_store_202100_to_202330.sql
@@ -2,7 +2,7 @@
 CREATE VIEW retail_202100_202330 AS
 SELECT	a.qty, a.cogsusd, a.revenueusd, 	
 		b.*,
-		c.storecode, c.channel, c.citylevel, c.areagroup, c.urbanization, c.branch, c.active, c.storelevel,
+		c.storecode, c.channel, c.citylevel, c.areagroup, c.urbanization, c.branch, c.active, c.storelevel, c.showroomarea, c.warehousearea,
 		d.*
 FROM sales a
 INNER JOIN product b
